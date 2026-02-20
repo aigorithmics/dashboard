bd004440ecab1e06f06334aef48904b07c50465b
magdalena.aigo <magdalena.kozub@aigorithmics.com>
2026-02-20 18:21:10 +0100

    Migrate Angular 17->18

--- components/centraldashboard-angular/frontend/webpack.config.js
+++ components/centraldashboard-angular/frontend/webpack.config.js
@@ -1,9 +1,11 @@
 'use strict';
-
-const {resolve} = require('path');
-const {execSync} = require('child_process');
-const DefinePlugin = require('webpack').DefinePlugin;
+import ESLintPlugin from 'eslint-webpack-plugin';
+import { resolve, dirname } from 'path';
+import { execSync } from 'child_process';
+import webpack from 'webpack';
+import version from './package.json' with { type: "json" };
 let commit = process.env.BUILD_COMMIT || '';
+const __dirname = import.meta.dirname;
 
 try {
     commit = commit || `${execSync(`git rev-parse HEAD`)}`.replace(/\s/g, '');
@@ -12,12 +14,12 @@ try {
 const ENV = process.env.NODE_ENV || 'development';
 const NODE_MODULES = /\/node_modules\//;
 const PKG_VERSION =
-    `${require('./package.json').version}-${commit.slice(0, 6)}`;
+    `${version}-${commit.slice(0, 6)}`;
 const BUILD_VERSION = process.env.BUILD_VERSION || `dev_local`;
 const SRC = resolve(__dirname, 'public');
 const DESTINATION = resolve(__dirname, 'dist', 'public');
 
-module.exports = {
+export default {
     mode: ENV,
     entry: {
         dashboard_lib: resolve(SRC, 'library.js'),
@@ -31,19 +33,6 @@ module.exports = {
     devtool: 'cheap-source-map',
     module: {
         rules: [
-            {
-                enforce: 'pre',
-                test: /\.js$/,
-                exclude: NODE_MODULES,
-                use: {
-                    loader: 'eslint-loader',
-                    options: {
-                        extends: ['eslint:recommended', 'google'],
-                        failOnError: true,
-                        fix: true,
-                    },
-                },
-            },
             {
                 test: /\.js$/,
                 exclude: NODE_MODULES,
@@ -66,14 +55,33 @@ module.exports = {
                                 },
                             },
                         ]],
-                        plugins: ['@babel/plugin-transform-runtime'],
+                        plugins:[
+                            ["@babel/plugin-transform-runtime", {
+                            "key": "ESLintWebpackPlugin",
+                            "options": {
+                                "cache": true,
+                                "cacheLocation": "node_modules/.cache/eslint-webpack-plugin/.eslintcache",
+                                "configType": "flat",
+                                "extensions": "js",
+                                "emitError": true,
+                                "emitWarning": true,
+                                "failOnError": true,
+                                "resourceQueryExclude": [],
+                                "extends": [
+                                "eslint:recommended",
+                                "google"
+                                ],
+                                "fix": true
+                            }
+                            }]
+                            ]
                     },
                 },
             },
         ],
     },
     plugins: [
-        new DefinePlugin({
+        new webpack.DefinePlugin({
             BUILD_VERSION: JSON.stringify(BUILD_VERSION),
             VERSION: JSON.stringify(PKG_VERSION),
         }),
